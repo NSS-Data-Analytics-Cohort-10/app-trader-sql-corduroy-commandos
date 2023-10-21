01936a1cf618601145b5da258c97817f35c6e07d
TomMaster <hercules_masteriv@hotmail.com>
2023-10-21 10:45:46 -0500

    app trader 20231021

--- Scripts/app_trader.sql
+++ Scripts/app_trader.sql
@@ -1,7 +1,17 @@
--- Based on research completed prior to launching App Trader as a company, you can assume the following:
--- a. App Trader will purchase apps for 10,000 times the price of the app. For apps that are priced from free up to $1.00, the purchase price is $10,000.
- 
 WITH X AS (
+
+SELECT 
+	O.AVG_RATING,
+	O.EXPECTED_YEARS,
+	O.TOTAL_MONTHS,
+	O.MARKETING_MONTHS,
+	O.TOTAL_REVENUE_OVER_MONTHS,
+	O.APP_COST,
+	O.TOTAL_PROFIT,
+	O.APP_NAME,
+    RANK () OVER (ORDER BY O.TOTAL_PROFIT DESC,O.APP_NAME) RANK_APP,
+	M.GENRE
+	FROM (
 SELECT 
 	W.AVG_RATING,
 	ROUND(W.AVG_RATING/.5+1) EXPECTED_YEARS,
@@ -24,7 +34,6 @@ SELECT
 	WHEN T.PRICE_APP < T.PRICE_PLAY THEN T.PRICE_PLAY
 	END AS MONEY) APP_COST,
 	
-	
 	CAST(
 	ROUND(W.AVG_RATING/.5+1)*12 *
 	CASE
@@ -32,20 +41,20 @@ SELECT
 	ELSE COALESCE(COUNT_APP,0)*5000
 	END
 	+
+	ROUND(W.AVG_RATING/.5+1)*12 *
 	CASE
 	WHEN COALESCE(Z.COUNT_PLAY,0) = 0 THEN 0
 	ELSE COALESCE(Z.COUNT_PLAY,0)*5000
 	END 
 	-
-	ROUND(W.AVG_RATING/.5+1)*12*1000
-	-
+	ROUND(W.AVG_RATING/.5+1)*12*1000 
+    -
 	CASE 
 	WHEN T.PRICE_APP = T.PRICE_PLAY THEN T.PRICE_APP
 	WHEN T.PRICE_APP > T.PRICE_PLAY THEN T.PRICE_APP
 	WHEN T.PRICE_APP < T.PRICE_PLAY THEN T.PRICE_PLAY
 	END AS MONEY) TOTAL_PROFIT,
-	
-	
+		
 	COALESCE(Y.COUNT_APP,0)COUNT_APP,
 	CASE
 	WHEN COALESCE(COUNT_APP,0) = 0 THEN 0
@@ -117,63 +126,25 @@ FROM
 UNION
 SELECT 
 	UPPER(TRIM(NAME))  APP_NAME_AVG,
-	RATING 
+	RATING
 FROM
 	PLAY_STORE_APPS
 )
 WHERE 
-	RATING IS NOT NULL
+  RATING IS NOT NULL
 GROUP BY 
 APP_NAME_AVG) W ON W.APP_NAME_AVG = T.APP_NAME
-)
-
-SELECT 
-	AVG_RATING,
-	EXPECTED_YEARS,
-	TOTAL_MONTHS,
-	MARKETING_MONTHS,
-	TOTAL_REVENUE_OVER_MONTHS,
-	APP_COST,
-	TOTAL_PROFIT,
-	APP_NAME,
-    RANK () OVER (ORDER BY TOTAL_PROFIT DESC,APP_NAME) RANK_APP
-FROM X
+) O
+INNER JOIN 
+(SELECT
+	UPPER(TRIM(NAME)) GENRE_APP_NAME,
+	UPPER(PRIMARY_GENRE)  GENRE
+FROM
+	APP_STORE_APPS) M ON O.APP_NAME = M.GENRE_APP_NAME
 WHERE 
-COUNT_APP >= 1 AND COUNT_PLAY >= 1 AND
-RANKING = 1 ORDER BY RANK_APP 
----ORDER BY 7 DESC; 
+--COUNT_APP >= 1 AND COUNT_PLAY >= 1 
+	--AND
+RANKING = 1 ORDER BY RANK_APP)
 
-/*
-SELECT * FROM APP_STORE_APPS  LIMIT (10);
-SELECT * FROM PLAY_STORE_APPS LIMIT (10);
-*/
--- - For example, an app that costs $2.00 will be purchased for $20,000.
-    
--- - The cost of an app is not affected by how many app stores it is on. A $1.00 app on the Apple app store will cost the same as a $1.00 app on both stores. 
-    
--- - If an app is on both stores, it's purchase price will be calculated based off of the highest app price between the two stores. 
+SELECT * FROM X WHERE RANK_APP <= 10
 
--- b. Apps earn $5000 per month, per app store it is on, from in-app advertising and in-app purchases, regardless of the price of the app.
-    
--- - An app that costs $200,000 will make the same per month as an app that costs $1.00. 
-
--- - An app that is on both app stores will make $10,000 per month. 
-
--- c. App Trader will spend an average of $1000 per month to market an app regardless of the price of the app. If App Trader owns rights to the app in both stores, it can market the app for both stores for a single cost of $1000 per month.
-    
--- - An app that costs $200,000 and an app that costs $1.00 will both cost $1000 a month for marketing, regardless of the number of stores it is in.
-
--- d. For every half point that an app gains in rating, its projected lifespan increases by one year. In other words, an app with a rating of 0 can be expected to be in use for 1 year, an app with a rating of 1.0 can be expected to last 3 years, and an app with a rating of 4.0 can be expected to last 9 years.
-
--- - App store ratings should be calculated by taking the average of the scores from both app stores and rounding to the nearest 0.5.
-
--- e. App Trader would prefer to work with apps that are available in both the App Store and the Play Store since they can market both for the same $1000 per month.
-
-
--- #### 3. Deliverables
-
--- a. Develop some general recommendations as to the price range, genre, content rating, or anything else for apps that the company should target.
-
--- b. Develop a Top 10 List of the apps that App Trader should buy.
-
--- c. Submit a report based on your findings. All analysis work must be done using PostgreSQL, however you may export query results to create charts in Excel for your report. 
